2eedac7b21d71856ff1e17de907b58074647acb6
Bruno-H-Guedes <bruno.guedes@aluno.ufop.edu.br>
2022-08-12 14:58:07 -0300

    Aula pratica 2

--- src/App.java
+++ src/App.java
@@ -7,9 +7,20 @@ public class App {
         g1.addEdge(3, 0, 4);
         g1.addEdge(3, 4, 4); // warning
         System.out.println(g1);
+        System.out.println("___________________________");
+        System.out.println("Degree of each node");
         System.out.println(g1.degree(0));
         System.out.println(g1.degree(1));
         System.out.println(g1.degree(2));
         System.out.println(g1.degree(3));
+        System.out.println("___________________________");
+        System.out.println("Highest degree: ");
+        System.out.println(g1.highestDegree());
+        System.out.println("___________________________");
+        System.out.println("Lowest degree: ");
+        System.out.println(g1.lowestDegree());
+        System.out.println("___________________________");
+        System.out.println(("Graph complemment"));
+        System.out.println(g1.complemment());
     }
 }
--- src/Graph.java
+++ src/Graph.java
@@ -52,4 +52,39 @@ public class Graph {
         }
         return degree;
     }
+
+    public int highestDegree(){
+        int highest =0;
+        for(int i=0; i < this.adjMatrix.length; i++){
+            int degreeNodeI = this.degree(i);
+            if(highest < degreeNodeI){
+                highest = degreeNodeI;
+            }
+        }
+        return highest;
+    }
+
+    public int lowestDegree(){
+        int lowest =this.countEdges;
+        for(int i=0; i < this.adjMatrix.length; i++){
+            int degreeNodeI = this.degree(i);
+            if(lowest > degreeNodeI){
+                lowest = degreeNodeI;
+            }
+        }
+        return lowest;
+    }
+
+    public Graph complemment(){
+        Graph g2 = new Graph(this.countNodes);
+        
+        for(int i=0; i < this.adjMatrix.length; i++){
+            for(int j=0; j < this.adjMatrix[i].length; j++){
+                if(i!=j && this.adjMatrix[i][j] == 0){
+                    g2.addEdge(i, j, 1);
+                }
+            }
+        }
+        return g2;
+    }
 }
